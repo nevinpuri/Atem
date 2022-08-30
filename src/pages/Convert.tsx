691acfc4958ea8808a1007a160998e8d5ab4377d
Nevin1901 <nevinpuri1901@gmail.com>
2022-08-30 21:56:34 +0200

    before test

--- src/pages/Convert.tsx
+++ src/pages/Convert.tsx
@@ -1,8 +1,26 @@
 import { useParams } from "react-router-dom";
 import { Buffer } from "buffer";
+import { useEffect } from "react";
+import { message } from "@tauri-apps/api/dialog";
+import { invoke } from "@tauri-apps/api";
 
 export default function Convert() {
   const params = useParams();
+  useEffect(() => {
+    const convertVideo = async () => {
+      if (!params.filePath) {
+        message("No file");
+        return;
+      }
+
+      const filePath = Buffer.from(params.filePath, "base64").toString(
+        "binary"
+      );
+
+      await invoke("convert_video", { input: filePath, target_size: 7.8 });
+      message("done");
+    };
+  }, []);
   return (
     <div>
       <h1>hello</h1>
