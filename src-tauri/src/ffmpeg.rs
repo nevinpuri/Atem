288862f9a5be137b0564d9924d78d4552cdb1bfc
Nevin1901 <nevinpuri1901@gmail.com>
2022-08-30 21:41:36 +0200

    more functions

--- src-tauri/src/ffmpeg.rs
+++ src-tauri/src/ffmpeg.rs
@@ -31,3 +31,92 @@ pub fn get_duration(input: &str) -> f32 {
 
         parsed
 }
+
+/// Returns in kb
+pub fn get_original_audio_rate(input: &str) -> f32 {
+    let output = Command::new("ffprobe")
+        .args(
+        ["-v", "error",
+        "-select_streams", "a:0",
+        "-show_entries", "stream=bit_rate",
+        "-of", "csv=p=0",
+        input])
+        .output()
+        .expect("Failed to run ffprobe to get original audio rate")
+        .stdout;
+    
+    let arate = match from_utf8(&output) {
+        Ok(value) => remove_whitespace(value),
+        Err(e) => {
+            eprintln!("{}", e);
+            panic!("Failed")
+        }
+    };
+
+    if arate == "N/A" {
+        return 0.00;
+    }
+
+    let parsed: f32 = arate.parse::<f32>().expect("Failed to parse original audio rate") / 1024.00;
+
+    println!("arate: {}", arate);
+
+    parsed
+    // use 7.8
+}
+
+pub fn get_target_size(audio_rate: f32, duration: f32) -> f32 {
+    let size = (audio_rate * duration) / 8192.00;
+    size
+}
+
+pub fn is_minsize(min_size: f32, size: f32) -> bool {
+    return min_size < size
+}
+
+/// returns in kib/s
+pub fn get_target_video_rate(size: f32, duration: f32, audio_rate: f32) -> f32 {
+    let size = (size * 8192.00) / (1.048576 * duration) - audio_rate;
+    size
+}
+
+pub fn convert_first(input: &str, video_bitrate: f32, unix: bool) {
+    let nul = if unix {
+        "/dev/null"
+    } else {
+        "nul"
+    };
+
+    let output = Command::new("ffmpeg")
+    .args([
+        "-y",
+        "-i", input,
+        "-c:v", "libx264",
+        "-b:v", format!("{}k", video_bitrate).as_str(),
+        "-pass", "1",
+        "-an",
+        "-f", "mp4",
+        nul
+    ])
+    .output()
+    .expect("Failed first conversion")
+    .stderr;
+
+    println!("{}", from_utf8(&output).unwrap());
+}
+
+pub fn convert_out(input: &str, video_bitrate: f32, audio_bitrate: f32, output: &str) {
+    let output = Command::new("ffmpeg")
+    .args([
+        "-i", input,
+        "-c:v", "libx264",
+        "-b:v", format!("{}k", video_bitrate).as_str(),
+        "-pass", "2",
+        "-c:a", "aac",
+        "-b:a", format!("{}k", audio_bitrate).as_str(),
+        output
+    ])
+    .output()
+    .expect("Failed first conversion")
+    .stdout;
+}
--- src-tauri/tests/mod.rs
+++ src-tauri/tests/mod.rs
@@ -1,7 +1,38 @@
-use compressor::ffmpeg::get_duration;
+use compressor::ffmpeg::*;
 
 #[test]
-fn test_duration() {
-    assert_eq!(get_duration("/home/nevin/Desktop/ffmpeg/teaser.mkv"), 30.033);
+fn test_utils() {
+    let input = "/home/nevin/Desktop/ffmpeg/teaser.mkv";
+    assert_eq!(get_duration(&input), 30.033);
+    assert_eq!(get_original_audio_rate(&input), 0.00);
     // get_duration("/home/nevin/Desktop/ffmpeg/teaser.mkv");
+}
+
+#[test]
+fn test_target_size() {
+    let input = "/home/nevin/Desktop/ffmpeg/teaser.mkv";
+    let duration = get_duration(input);
+    let audio_rate = get_original_audio_rate(input);
+    let target_size = get_target_size(audio_rate, duration);
+    println!("target size: {}", target_size);
+}
+
+#[test]
+fn test_convert() {
+    let input = "/home/nevin/Desktop/ffmpeg/teaser.mkv";
+    let target_size = 7.8;
+    let duration = get_duration(input);
+    let audio_rate = get_original_audio_rate(input);
+    let min_size = get_target_size(audio_rate, duration);
+
+    if !is_minsize(min_size, target_size) {
+        println!("Target size too small");
+        return;
+    }
+
+    let target_bitrate = get_target_video_rate(target_size, duration, audio_rate);
+    convert_first(input, target_bitrate, true);
+    convert_out(input, target_bitrate, audio_rate, "/home/nevin/Desktop/ffmpeg/teaser-8m.mp4");
+    // write to file and reread that file every second
+    println!("done converting");
 }
