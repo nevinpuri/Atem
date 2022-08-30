0fc68a6195f77a658d8b3ab4c9d348b9818cf47c
Nevin1901 <nevinpuri1901@gmail.com>
2022-08-30 21:53:57 +0200

    added command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,28 @@
+use compressor::ffmpeg::{get_duration, get_original_audio_rate, get_target_size, is_minsize, get_target_video_rate, convert_first, convert_out};
+
 pub mod ffmpeg;
 
 #[cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+
+#[tauri:command]
+fn convert_video(input: &str, target_size: f32) ->  bool {
+    let output = format!("{}-8m.mp4", input).as_str();
+    let duration = get_duration(input);
+    let audio_rate = get_original_audio_rate(input);
+    let min_size = get_target_size(audio_rate, duration);
+
+    if !is_minsize(min_size, size) {
+        return false;
+    }
+
+    let target_bitrate = get_target_video_rate(size, duration, audio_rate);
+    convert_first(input, target_bitrate, true);
+    convert_out(input, target_bitrate, audio_bitrate, output);
+    true
+}
 
 #[tauri::command]
 fn greet(name: &str) -> String {
