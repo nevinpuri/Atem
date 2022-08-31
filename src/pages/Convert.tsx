3c8fa5a30461efe4d6050b581f0282f2b74f7e3e
Nevin1901 <nevinpuri1901@gmail.com>
2022-08-31 21:40:45 +0200

    added error checking and status

--- src/pages/Convert.tsx
+++ src/pages/Convert.tsx
@@ -1,24 +1,34 @@
 import { useParams } from "react-router-dom";
 import { Buffer } from "buffer";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { message } from "@tauri-apps/api/dialog";
 import { invoke } from "@tauri-apps/api";
 import debounce from "lodash.debounce";
 
 export default function Convert() {
+  const [convertStatus, setConvertStatus] = useState<string>("Converting");
   const params = useParams();
 
   const convertVideo = async () => {
-    console.log("converting");
-    if (!params.filePath) {
-      message("No file");
-      return;
+    try {
+      console.log("converting");
+      if (!params.filePath) {
+        message("No file");
+        return;
+      }
+
+      const filePath = Buffer.from(params.filePath, "base64").toString(
+        "binary"
+      );
+
+      await invoke("convert_video", { input: filePath, targetSize: 7.8 });
+      setConvertStatus("Successfully converted");
+      // open folder
+    } catch (err) {
+      setConvertStatus(
+        "An unexpected error has occured. Check the console for details."
+      );
     }
-
-    const filePath = Buffer.from(params.filePath, "base64").toString("binary");
-
-    await invoke("convert_video", { input: filePath, targetSize: 7.8 });
-    message("done");
   };
 
   const debouncedEventHandler = useMemo(() => debounce(convertVideo, 300), []);
@@ -26,13 +36,9 @@ export default function Convert() {
     debouncedEventHandler();
   }, []);
   return (
-    <div>
-      <h1>hello</h1>
-      <h1 className="text-white">
-        {Buffer.from(
-          params.filePath ? params.filePath : "hello",
-          "base64"
-        ).toString("binary")}
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-white text-2xl font-bold text-center">
+        {convertStatus}
       </h1>
     </div>
   );
